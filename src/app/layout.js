2f56c8dd192739b327c37906f4f56963c64d69f3
jinzhepro <jinzhepro@qq.com>
2025-08-22 16:07:11 +0800

    chore: 移除vConsole调试工具及其相关依赖
    清理不再使用的移动端调试工具vConsole及相关代码
    移除package.json中的vconsole依赖项
    删除VConsole组件文件

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import VConsole from "@/components/VConsole";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <VConsole /> */}
+
         {children}
       </body>
     </html>
